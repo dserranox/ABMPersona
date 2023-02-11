77ea28ed9dffd4d01742bdd1f9b10d675d40d485
dserrano <dserrano@trimix.com.ar>
2023-02-11 12:22:12 -0300

    first commit

---
+++ src/main/java/com/example/ABMPersona/AbmPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ABMPersona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmPersonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ABMPersona/AbmPersonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ABMPersona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmPersonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
